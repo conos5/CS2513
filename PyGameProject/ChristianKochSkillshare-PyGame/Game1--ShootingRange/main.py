34d6643454ec194c877fb48a6e08d35f94ee9271
Conor Shipsey <cpshipsey@gmail.com>
2022-11-20 22:44:01 +0000

    Began shooting range game

--- PyGameProject/ChristianKochSkillshare-PyGame/Game1--ShootingRange/main.py
+++ PyGameProject/ChristianKochSkillshare-PyGame/Game1--ShootingRange/main.py
@@ -1,6 +1,20 @@
 import pygame
+import sys
 
 pygame.init()
-
 screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
 
+wood_bg = pygame.image.load('GameAssets/Wood_BG.png')
+land_bg = pygame.image.load('GameAssets/Land_BG.png')
+
+while True:  # speed of update i.e frame rate is not set!
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.blit(wood_bg, (0, 0))  # (0, 0) = top left !
+    screen.blit(land_bg, (0, 560))
+    pygame.display.update()
+    clock.tick(120)  # stops framerate from exceeding 120 FPS
